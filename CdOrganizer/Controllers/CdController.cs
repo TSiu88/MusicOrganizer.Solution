a94f462bd340fca7217d767e1779d63cf12fd3a5
Jiwon <jiwon1.han@gmail.com>
2020-03-12 08:45:30 -0700

    Set up for the project

---
+++ CdOrganizer/Controllers/CdController.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using musicOrganizer.Models;
+
+
+namespace musicOrganizer.Controllers
+{
+    public class musicOrganizer : Controller
+    {
+      
+ 
+    }
+}
---
+++ CdOrganizer/Models/Artist.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace musicOrganizer.Models
+{
+    public class Artist
+    {
+      
+    }
+}
